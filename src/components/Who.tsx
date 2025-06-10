b559d0d9407e49de28383cc8b16e08807aa64f82
shrimpwarrior <thomaslenh@gmail.com>
2025-06-10 09:07:25 -0400

    updated

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -3,7 +3,6 @@ import logo from "../assets/logo.png";
 const Who = () => {
   return (
     <div className="bg-[#F8F8FF] w-full flex flex-col md:flex-row justify-center items-center p-4 shadow h-auto gap-4">
-      {/* Heading section - placed outside the main box on larger screens */}
       <div className="text-sm md:text-2xl lg:text-3xl flex gap-1 tracking-tight md:flex-col md:items-center md:w-full ">
         <h1 className="font-medium md:font-semibold">Who is</h1>
         <h1 className="text-orange-500 drop-shadow-lg font-semibold md:font-bold  text-shadow-lg/30">
@@ -11,7 +10,6 @@ const Who = () => {
         </h1>
       </div>
 
-      {/* Content box */}
       <div className="bg-gradient-to-b md:flex md:justify-around md:items-center from-[#F0F8FF] via-[#F0F8FF] to-orange-50 w-full max-w-lg rounded-3xl p-4 shadow-xl ">
         <div className="text-[0.65rem]/5 text-gray-600 font-medium md:text-base/6 lg:text-base/10 text-left">
           <p>☑ Irish Protestant.</p>
